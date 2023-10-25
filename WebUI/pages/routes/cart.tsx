356ea32829236b0af70d57d310157355ce04c920
Felipe Andreoli Ferrarias <felipe_andreoli90@hotmail.com>
2023-10-25 17:43:05 -0300

    Removido Front End

--- WebUI/pages/routes/cart.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Cart() {
-  return (
-    <div>
-      <p className="text-center text-xl pt-10">Meu Carrinho</p>
-    </div>
-  )
-}
--- WebUI/remix.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-export default {
-  ignoredRouteFiles: ["**/.*"],
-  appDirectory: "pages",
-  tailwind: true,
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
-};
